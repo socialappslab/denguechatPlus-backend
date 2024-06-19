a802b8b653886eace53eda88083173c78b774453
Raul Ferreira Goebel <ferreirarauldev@gmail.com>
2024-06-19 14:26:04 -0400

    Add frozen string literals and update seeds.rb
    Added `# frozen_string_literal: true` to migration files for string immutability. Also, updated the `seeds.rb` file to create a default user profile and associated account data, fetching the default password value from environment variables. This will streamline the database seeding process.

--- db/migrate/20240618010004_create_user_accounts.rb
+++ db/migrate/20240618010004_create_user_accounts.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateUserAccounts < ActiveRecord::Migration[7.1]
   def change
     create_table :user_accounts do |t|
--- db/migrate/20240618015203_add_resource_reference_to_role.rb
+++ db/migrate/20240618015203_add_resource_reference_to_role.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddResourceReferenceToRole < ActiveRecord::Migration[7.1]
   def change
     add_reference :roles, :resource, polymorphic: true
