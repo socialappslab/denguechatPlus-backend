2c5c6648a1548a28ff1bb101023fea08ab4db812
Raul Ferreira Goebel <ferreirarauldev@gmail.com>
2024-06-28 10:14:29 -0400

    Refactor session destroy operation
    The operations/destroy.rb file under the users/sessions module has been significantly refactored. Incorporation of Dry::Transaction was included for better handling of the operation flow. The new approach improves error handling and provides a more thorough and efficient session deletion process.

--- app/concepts/api/v1/users/sessions/operations/destroy.rb
+++ app/concepts/api/v1/users/sessions/operations/destroy.rb
@@ -6,12 +6,28 @@ module Api
       module Sessions
         module Operations
           class Destroy < ApplicationOperation
+            include Dry::Transaction
+            include Inject[
+                      flush_session_by_token: 'sessions.flush_session_by_token'
+                    ]
+            tee :params
             step :flush_session
 
-            def flush_session(_ctx, flush_session:, found_token:, current_user:, **)
-              return unless current_user
+            def params(input)
+              @ctx = {}
+              @current_user = input[:current_user]
+              @found_token = input[:found_token]
+            end
 
-              flush_session.call(found_token, current_user)
+            def flush_session
+              return Failure({ ctx: @ctx, type: :gone }) unless @current_user && @found_token
+
+              result = flush_session_by_token.call(@found_token, @current_user)
+              if result&.positive?
+                Success({ ctx: @ctx, type: :destroyed })
+              else
+                Failure({ ctx: @ctx, type: :gone })
+              end
             end
           end
         end
