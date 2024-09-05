f131e6259998297845726f7e7369d04478d09d93
Raul Ferreira G <ferreirarauldev@gmail.com>
2024-09-05 11:39:23 -0400

    Make inspection fields optional
    Updated several fields in the inspection contract to be optional, including `container_test_result`, `in_use`, `tracking_type_required`, and `treated_by_id`. This change enhances flexibility in creating inspection records by not requiring data for these fields.

--- app/concepts/api/v1/visits/contracts/inspection.rb
+++ app/concepts/api/v1/visits/contracts/inspection.rb
@@ -7,14 +7,14 @@ module Api
         class Inspection < Dry::Validation::Contract
           params do
             optional(:code_reference).filled(:string)
-            required(:container_test_result).filled(:string)
+            optional(:container_test_result).filled(:string) #optional
             required(:has_lid).filled(:bool)
             required(:has_water).filled(:bool)
-            required(:in_use).filled(:bool)
-            required(:tracking_type_required).filled(:string)
+            optional(:in_use).filled(:bool) #optional
+            optional(:tracking_type_required).filled(:string) #optional
             required(:was_chemically_treated).filled(:bool)
             optional(:created_by_id).filled(:integer)
-            required(:treated_by_id).filled(:integer)
+            optional(:treated_by_id).filled(:integer) #optional
             required(:breeding_site_type_id).filled(:integer)
             required(:elimination_method_type_id).filled(:integer)
             required(:water_source_type_id).filled(:integer)
