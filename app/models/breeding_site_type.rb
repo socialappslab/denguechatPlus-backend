4630041cf1c19398d05047396a0978fa809d1a36
Raul Ferreira G <ferreirarauldev@gmail.com>
2024-09-19 15:44:23 -0400

    Refactor container types to breeding site types
    This refactor converts all references from ContainerTypes to BreedingSiteTypes in both the data model and seed tasks. Additionally, it introduces a status color for options to indicate infection status, and adds a change_status permission for users.

--- app/models/breeding_site_type.rb
+++ app/models/breeding_site_type.rb
@@ -2,11 +2,12 @@
 #
 # Table name: breeding_site_types
 #
-#  id           :bigint           not null, primary key
-#  discarded_at :datetime
-#  name         :string
-#  created_at   :datetime         not null
-#  updated_at   :datetime         not null
+#  id             :bigint           not null, primary key
+#  container_type :string
+#  discarded_at   :datetime
+#  name           :string
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
 #
 class BreedingSiteType < ApplicationRecord
   include Discard::Model
