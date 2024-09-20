7d7ee0893005acc4a8b72f8eeac762f8e39868b5
Raul Ferreira G <ferreirarauldev@gmail.com>
2024-09-19 20:46:37 -0400

    Add Rollbar for error tracking
    Configured Rollbar for asynchronous error logging in Rails. Integrated synchronous fallback in case of async failure.

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,24 @@
 # frozen_string_literal: true
 
-# Load the Rails application.
-require_relative 'application'
+# config/environment.rb
 
-# Initialize the Rails application.
-Rails.application.initialize!
+require File.expand_path('../application', __FILE__)
+require File.expand_path('../rollbar', __FILE__)
+
+notify = ->(e) do
+  begin
+    Rollbar.with_config(use_async: false) do
+      Rollbar.error(e)
+    end
+  rescue
+    Rails.logger.error "Synchronous Rollbar notification failed.  Sending async to preserve info"
+    Rollbar.error(e)
+  end
+end
+
+begin
+  Rails.application.initialize!
+rescue Exception => e
+  notify.(e)
+  raise
+end
