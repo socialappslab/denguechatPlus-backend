7bdcf8bf0a8895ce3161a44562003e121bd621b7
Raul Ferreira G <ferreirarauldev@gmail.com>
2024-10-09 15:45:43 -0300

    add migration for apply the changes on the seed without run a rake task

---
+++ db/migrate/20241009183008_change_options_types_of_the_question_about_photo.rb
@@ -0,0 +1,30 @@
+class ChangeOptionsTypesOfTheQuestionAboutPhoto < ActiveRecord::Migration[7.1]
+  def up
+    questionOtherContainers = Question.find_by(question_text_es: '¿Hay otros contenedores como este?')
+    if questionOtherContainers
+      options = questionOtherContainers.options
+      option = options.find{|option| option.name_es == 'No'}
+      if option
+        option.type_option = 'boolean'
+        option.value = 0
+        option.save!
+      end
+    end
+    questionPhotos = Question.find_by(question_text_es: '¿Puedes subir una foto del tipo de contenedor?')
+    if questionPhotos
+      options = questionPhotos.options
+      option_yes = options.find{|option| option.name_es == 'Si, si puedo'}
+      option_no = options.find{|option| option.name_es == 'No, no no puedo'}
+      if option_yes
+        option_yes.type_option = 'boolean'
+        option_yes.value = 0
+        option_yes.save!
+      end
+      if option_no
+        option_no.type_option = 'boolean'
+        option_no.value = 1
+        option_no.save!
+      end
+    end
+  end
+end
