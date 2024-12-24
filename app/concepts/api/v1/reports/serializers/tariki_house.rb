9b57c5e9ba3d2bc917baaf86dcfaa6627839a350
Raul Ferreira G <ferreirarauldev@gmail.com>
2024-12-24 02:59:46 -0300

    fix: Fix the tariki houses endpoint

--- app/concepts/api/v1/reports/serializers/tariki_house.rb
+++ app/concepts/api/v1/reports/serializers/tariki_house.rb
@@ -12,7 +12,7 @@ module Api
           attributes :total_houses_qty, :tariki_houses_qty
 
           attribute :tariki_percentage do |report|
-            next 0 if report.total_houses_qty.blank?
+            next 0 if report.total_houses_qty.blank? || report.total_houses_qty < 1
 
             (report.tariki_houses_qty.to_f / report.total_houses_qty.to_f) * 100
           end
@@ -20,7 +20,7 @@ module Api
           attributes :green_container_qty, :total_container_qty
 
           attribute :green_container_percentage do |report|
-            next 0 if report.total_container_qty.blank?
+            next 0 if report.total_container_qty.blank? || report.green_container_qty < 1
 
             (report.green_container_qty.to_f / report.total_container_qty.to_f) * 100
           end
