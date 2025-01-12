f54431ed9049d80b52bcc034b18c6adf98af3479
Raul Ferreira G <ferreirarauldev@gmail.com>
2025-01-12 16:25:03 -0300

    changes by visits and inspections, (update and list)

--- app/concepts/api/v1/inspections/contracts/update.rb
+++ app/concepts/api/v1/inspections/contracts/update.rb
@@ -1,52 +1,86 @@
 # frozen_string_literal: true
-require 'ostruct'
 
 module Api
   module V1
     module Inspections
       module Contracts
         class Update < Dry::Validation::Contract
-
           def self.kall(...)
             new.call(...)
           end
 
           params do
-            required(:id).filled(:integer)
-            required(:post_id).filled(:integer)
-            optional(:user_account_id).filled(:integer)
-            optional(:content).filled(:string, min_size?: 1, max_size?: 280)
-            optional(:photo)
+            optional(:id)
+            optional(:breeding_site_type_id).filled(:integer)
+            optional(:treated_by_id).filled(:integer)
+
+            optional(:code_reference).maybe(:string)
+            optional(:container_test_result).maybe(:string)
+            optional(:has_water).maybe(:bool)
+            optional(:lid_type).maybe(:string)
+            optional(:lid_type_other).maybe(:string)
+            optional(:other_elimination_method).maybe(:string)
+            optional(:other_protection).maybe(:string)
+            optional(:was_chemically_treated).maybe(:string)
+            optional(:water_source_other).maybe(:string)
+
+            optional(:container_protection_id).maybe(:integer)
+            optional(:elimination_method_type_id).maybe(:integer)
+            optional(:water_source_type_id).maybe(:integer)
+            optional(:type_content_ids).maybe(:array)
+
             optional(:delete_photo).maybe(:bool)
+            optional(:photo)
+
+            optional(:page).maybe(:hash) do
+              optional(:is_cursor).maybe(:bool)
+            end
+
+            optional(:sort).maybe(:string)
           end
 
           rule(:id) do
-            unless Comment.find_by(id: values[:id])
-              key(:id).failure(text: 'The comment not exists', predicate: :not_found?)
+            unless Inspection.exists?(id: value)
+              key.failure(text: "The inspection does not exist", predicate: :not_exists?)
             end
           end
 
-          rule(:user_account_id) do
-            unless UserAccount.find_by(id: values[:user_account_id])
-              key(:user_account_id).failure(text: 'the user not exist', predicate: :not_found?)
+          rule(:breeding_site_type_id) do
+            unless BreedingSiteType.exists?(id: value)
+              key.failure(text: "The BreedingSiteType does not exist", predicate: :not_exists?)
             end
           end
 
-          rule(:post_id) do
-            unless Post.find_by(id: values[:post_id])
-              key(:user_account_id).failure(text: 'the post not exist', predicate: :not_found?)
-            end
-          end
-
-          rule(:photo) do
-            if values && !values[:photo].nil? && !values[:photo].is_a?(ActionDispatch::Http::UploadedFile)
-              unless values[:photo].content_type.start_with?('image/')
-                key(:photo).failure(text: 'must be an image')
+          rule(:elimination_method_type_id) do
+            if key?
+              unless EliminationMethodType.exists?(id: value)
+                key.failure(text: "The EliminationMethodType does not exist", predicate: :not_exists?)
               end
+            end
+          end
 
-              if values[:photo].size > 5_242_880
-                key(:photo).failure(text: 'is too big, must be less than 5MB',
-                                    predicate: :unique?)
+          rule(:water_source_type_id) do
+            if key?
+              unless WaterSourceType.exists?(id: value)
+                key.failure(text: "The WaterSourceType does not exist", predicate: :not_exists?)
+              end
+            end
+          end
+
+          rule(:container_protection_id) do
+            if key?
+              unless ContainerProtection.exists?(id: value)
+                key.failure(text: "The ContainerProtection does not exist", predicate: :not_exists?)
+              end
+            end
+          end
+
+          rule(:type_content_ids) do
+            if key?
+              if value.nil?
+                key.failure(text: "The TypeContent does not exist", predicate: :not_exists?)
+              elsif !value.all? { |id| TypeContent.exists?(id: id) }
+                key.failure(text: "The TypeContent does not exist", predicate: :not_exists?)
               end
             end
           end
