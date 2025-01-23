c81cc50a524a4c3ce0f9728fc069b3b60d1be0d4
Raul Ferreira G <ferreirarauldev@gmail.com>
2025-01-23 15:25:56 -0300

    add some filters to sync logs view

--- app/controllers/web/sync_logs_controller.rb
+++ app/controllers/web/sync_logs_controller.rb
@@ -8,11 +8,28 @@ module Web
     before_action :http_authenticate
 
     def index
-      @pagy, @logs = pagy(SyncLog.includes(:sync_log_errors).order(created_at: :desc), items: 10)
+      scope = SyncLog.includes(:sync_log_errors).order(created_at: :desc)
+
+      if params[:start_date].present?
+        start_date = Date.parse(params[:start_date]).beginning_of_day
+        scope = scope.where("created_at >= ?", start_date)
+      end
+
+      if params[:end_date].present?
+        end_date = Date.parse(params[:end_date]).end_of_day
+        scope = scope.where("created_at <= ?", end_date)
+      end
+
+      @pagy, @logs = pagy(scope, items: 10)
     end
 
     def show
-      @pagy, @errors = pagy(SyncLogError.where(sync_log_id: params[:id]).order(created_at: :desc), items: 10)
+      scope = SyncLogError.where(sync_log_id: params[:id])
+
+      scope = scope.where("message ILIKE ?", "%#{params[:message]}%") if params[:message].present?
+      scope = scope.where(item_id: params[:item_id].upcase) if params[:item_id].present?
+
+      @pagy, @errors = pagy(scope.order(created_at: :desc), items: 10)
     end
 
   end
