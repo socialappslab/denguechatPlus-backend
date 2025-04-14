adb72735ed28f3e2de1312e6a0f1705b2fde3e96
fego-dev <ferreirarauldev@gmail.com>
2025-04-14 18:46:43 -0300

    I finished the download visits csv module

---
+++ app/concepts/api/v1/visits/contracts/download_information.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    module Visits
+      module Contracts
+        class DownloadInformation < Dry::Validation::Contract
+
+          def self.kall(...)
+            new.call(...)
+          end
+
+          params do
+            required(:id).filled(:integer)
+          end
+        end
+      end
+    end
+  end
+end
--- app/concepts/api/v1/visits/operations/download_csv.rb
+++ app/concepts/api/v1/visits/operations/download_csv.rb
@@ -9,18 +9,100 @@ module Api
         class DownloadCsv < ApplicationOperation
           include Dry::Transaction
 
+          tee :params
+          step :validate_params!
           step :generate_csv
 
-          def generate_csv(_input = nil)
-            csv_data = CSV.generate(headers: true) do |csv|
-              csv << %w[id has_water]
+          def params(input)
+            @ctx = {}
+            @params = to_snake_case(input[:params])
+          end
 
-              Visit.first.inspections.find_each do |inspection|
-                csv << [inspection.id, inspection.has_water]
-              end
+          def validate_params!
+            @ctx['contract.default'] = Api::V1::Visits::Contracts::DownloadInformation.kall(@params)
+            return Success({ ctx: @ctx, type: :success }) if @ctx['contract.default'].success?
+
+            Failure({ ctx: @ctx, type: :invalid })
+          end
+
+          def generate_csv(_input = nil)
+            visit = Visit.includes(:house, :user_account, :team, :inspections).find(@params[:id])
+
+            csv_data = CSV.generate(headers: true) do |csv|
+              csv << visit_headers
+              csv << visit_row(visit)
+
+              csv << []
+              csv << []
+
+              csv << inspection_headers
+              visit.inspections.each { |inspection| csv << inspection_row(inspection) }
             end
 
-            Success(csv_data)
+            Success(
+              send_data: {
+                data: csv_data,
+                filename: "visita_#{visit.id}.csv",
+                type: "text/csv"
+              },
+              type: :send_data
+            )
+          end
+
+          private
+
+          def visit_headers
+            [
+              "ref_sitio",
+              "fecha_visita",
+              "brigadista",
+              "brigada",
+              "permiso para visitar la casa",
+              "notas"
+            ]
+          end
+
+          def visit_row(visit)
+            [
+              visit.house&.reference_code,
+              visit.visited_at.strftime("%d/%m/%Y %H:%M"),
+              visit.user_account&.full_name,
+              visit.team&.name,
+              visit.visit_permission ? "Sí" : "No",
+              visit.notes
+            ]
+          end
+
+          def inspection_headers
+            %w[tipo_de_recipiente
+ hay_agua_en_el_recipiente
+origen_del_agua
+otro_origen_del_agua
+tipo_de_protección
+otro_tipo_de_protección
+fue_tratado_por_el_ministerio_de_salud
+en_el_recipiente_habia
+acción_realizada_sobre_el_recipiente
+otra_acción]
+          end
+
+          def inspection_row(inspection)
+            [
+              inspection.breeding_site_type.name&.gsub(',', '-'),
+              inspection.has_water,
+              inspection&.water_source_type&.name&.gsub(',', '-'),
+              inspection.water_source_other,
+              inspection.container_protections&.pluck(:name_es)&.join("-"),
+              inspection.other_protection,
+              bool(inspection.was_chemically_treated)&.gsub(',', '-'),
+              inspection.type_contents&.pluck(:name_es)&.join("-"),
+              inspection.elimination_method_type&.name_es&.gsub(',', '-'),
+              inspection.other_elimination_method
+            ]
+          end
+
+          def bool(value)
+            value ? "Sí" : "No"
           end
         end
       end
