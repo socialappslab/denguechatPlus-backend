ffbe2355f9a7094a59f9af4a23c6b004666e46c3
fego-dev <ferreirarauldev@gmail.com>
2025-05-30 03:20:55 -0300

    The term “recipiente/envase” is currently used across multiple views in the application. This phrasing is inconsistent and should be standardized to simply “envase” for clarity. Additionally, the English and Portuguese translations must be updated accordingly

---
+++ db/migrate/20250530060817_modify_term_recipiente_in_descritpion_options.rb
@@ -0,0 +1,55 @@
+class ModifyTermRecipienteInDescritpionOptions < ActiveRecord::Migration[7.1]
+  def up
+
+    execute <<~SQL
+      UPDATE questions
+      SET description_es = REPLACE(description_es, 'recipientes/envases', 'envases')
+      WHERE description_es ILIKE '%recipientes/envases%';
+    SQL
+
+    execute <<~SQL
+      UPDATE questions
+      SET description_es = REPLACE(description_es, 'recipiente/envase', 'envase')
+      WHERE description_es ILIKE '%recipiente/envase%';
+    SQL
+
+    execute <<~SQL
+      UPDATE questions
+      SET description_es = REPLACE(description_es, 'recipientes', 'envases')
+      WHERE description_es ILIKE '%recipientes%';
+    SQL
+
+    execute <<~SQL
+      UPDATE questions
+      SET description_es = REPLACE(description_es, 'recipiente', 'envase')
+      WHERE description_es ILIKE '%recipiente%';
+    SQL
+
+
+
+    execute <<~SQL
+      UPDATE options
+      SET name_es = REPLACE(name_es, 'recipientes/envases', 'envases')
+      WHERE name_es ILIKE '%recipientes/envases%';
+    SQL
+
+    execute <<~SQL
+      UPDATE options
+      SET name_es = REPLACE(name_es, 'recipiente/envase', 'envase')
+      WHERE name_es ILIKE '%recipiente/envase%';
+    SQL
+
+    execute <<~SQL
+      UPDATE options
+      SET name_es = REPLACE(name_es, 'recipiente', 'envase')
+      WHERE name_es ILIKE '%recipiente%';
+    SQL
+
+    execute <<~SQL
+      UPDATE options
+      SET name_es = REPLACE(name_es, 'recipientes', 'envases')
+      WHERE name_es ILIKE '%recipientes%';
+    SQL
+  end
+
+end
