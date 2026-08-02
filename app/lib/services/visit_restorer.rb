55c3fcb5fbbc0b49cd6bbd06a6462149a81c0d99
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2026-08-02 18:01:29 -0300

    Stop inferring points when restoring visits

--- app/lib/services/visit_restorer.rb
+++ app/lib/services/visit_restorer.rb
@@ -21,7 +21,6 @@ module Services
             house:,
             affected_dates: [VisitStateReconciler.reporting_date(visit.visited_at)]
           )
-          restore_points!
         end
       end
 
@@ -31,45 +30,5 @@ module Services
     private
 
     attr_reader :visit
-
-    def restore_points!
-      return unless point_eligible?
-
-      visit_id = visit.id
-      return if Point.exists?(visit_id:)
-
-      Api::V1::Points::Services::Transactions.assign_point(
-        earner: visit.user_account,
-        house_id: visit.house_id,
-        visit_id:
-      )
-    end
-
-    def point_eligible?
-      minimum = AppConfigParam.find_by(name: 'consecutive_green_statuses_for_tariki_house')&.value.to_i
-      return false unless minimum.positive?
-
-      statuses = statuses_ending_at_visit(minimum)
-      statuses.length >= minimum && statuses.all?(Constants::RiskColor::GREEN)
-    end
-
-    def statuses_ending_at_visit(limit)
-      house_id = visit.house_id
-      if AppConfigParam.exists?(name: 'tariki_point_same_date', value: 1)
-        Visit.where(house_id:)
-             .where(created_at: ..visit.created_at)
-             .order(created_at: :desc, id: :desc)
-             .limit(limit)
-             .pluck(:status)
-      else
-        date = VisitStateReconciler.reporting_date(visit.visited_at)
-        return [] unless date
-
-        HouseStatus.where(house_id:, date: ..date)
-                   .order(date: :desc, created_at: :desc, id: :desc)
-                   .limit(limit)
-                   .pluck(:status)
-      end
-    end
   end
 end
